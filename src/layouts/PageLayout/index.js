8f85d22b66624c16c7146fb307bc194191d650b0
Auzan <auzan.ip@gmail.com>
2022-03-08 08:49:46 +0700

    first commit

---
+++ src/layouts/PageLayout/index.js
@@ -0,0 +1,44 @@
+
+//NOT EDITABLE!!!
+import Topography from "../../components/Topography"
+import Hero from "../../components/Hero"
+import Footer from "../../components/Footer"
+
+// EDITABLE
+import background from "../../assets/img/background.jpg" //Background
+import backgroundMobile from "../../assets/img/backgroundM.jpg" //BackgroundMobile
+import logo from '../../assets/img/logo.png'; //Logo
+
+
+function pageLayout( props ) {
+    //EDITABLE
+    const deskripsi = "Selamat datang kembali di"
+    const title1 = "Bukittinggi"
+    const title2 = "Blended Learning"
+    const slogan = "Siapa yang menempuh jalan untuk mencari ilmu, maka Allah akan mudahkan baginya jalan menuju surga."
+    const copyright = "2021 Bukittinggi Blended Learning - Didukung oleh Geschool"
+
+    return (
+        <div>
+            <Topography />
+            <Hero 
+                logo={logo}
+                background={background}
+                backgroundMobile={backgroundMobile}
+                deskripsi={deskripsi}
+                title1={title1}
+                title2={title2}
+                slogan={slogan}
+                appName={props.appName}
+            >
+                { props.children }
+            </Hero>
+            <Footer
+                copyright={copyright}
+            />
+        </div>
+    );
+}
+
+
+export default pageLayout;
